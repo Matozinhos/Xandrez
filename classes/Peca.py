2bc7d92f1df882cb2b0bd8829bcb21b3e23a5bbe
Matozinhos <arthursoarespezzinimoreira@gmail.com>
2026-08-23 19:47:06 -0300

    Tem um erro em algum lugar

--- classes/Peca.py
+++ classes/Peca.py
@@ -42,6 +42,8 @@ class Peca: # SuperClass
 
         return movimentos
 
+# Subclasses
+
 class Peao(Peca):
     def __init__(self, cor, linha, coluna):
         super().__init__(cor, linha, coluna)
@@ -119,20 +121,51 @@ class Cavalo(Peca):
 class Rei(Peca):
     def __init__(self, cor, linha, coluna):
         super().__init__(cor, linha, coluna)
+        self.check = False
         self.__direcoes = [
             (-1, 0), (1, 0), (0, -1), (0, 1),
             (-1, -1), (-1, 1), (1, -1), (1, 1),
         ]
 
     def movimentos_validos(self, tabuleiro):
+
+        movimentos_impossiveis = []
+        for delta_linha, delta_coluna in self.__direcoes:
+            linha, coluna = self.__linha, self.__coluna
+            
+        while tabuleiro.dentro_dos_limite(linha, coluna):
+            linha += delta_linha
+            coluna += delta_coluna
+
+            peca_alvo = tabuleiro.get_peca(linha, coluna)
+            
+            if peca_alvo is None:
+                break
+            if peca_alvo.cor == self.cor:
+                break
+
+            if (delta_linha, delta_coluna) in [(-1, 0), (1, 0), (0, -1), (0, 1)] # se tiver aqui e é uma torre ou rainha == tmnc
+                if isinstance(peca_alvo, (Torre, Rainha)):
+                    movimentos_impossiveis.append((linha, coluna))
+            else:
+                if isinstance(peca_alvo, (Bispo, Rainha)):
+                    movimentos_impossiveis.append((linha,coluna))
+
         movimentos = []
 
         for delta_linha, delta_coluna in self.__direcoes:
+            atacada = False 
             linha, coluna = self.__linha + delta_linha, self.__coluna + delta_coluna
 
-            if not tabuleiro.dentro_dos_limite(linha, coluna):
+            if not tabuleiro.dentro_dos_limite(linha, coluna) or (linha,coluna) in movimentos_impossiveis:
                 continue
+
+            for delta_l, delta_c in tabuleiro.get_casas_atacadas() :
+                if (linha, coluna) == (delta_l, delta_c):
+                    continue
 
             peca_alvo = tabuleiro.get_peca(linha, coluna)
             if peca_alvo is None or peca_alvo.cor != self.cor:
                 movimentos.append((linha, coluna))
+
+        return movimentos
--- classes/Tabuleiro.py
+++ classes/Tabuleiro.py
@@ -1,9 +1,15 @@
+from Peca import Peca
+
 class Tabuleiro:
     def __init__(self):
-        self.MatrizTabuleiro = [[ () for _ in range(8)] for _ in range(8)]
+        self.matrizTabuleiro = [[ () for _ in range(8)] for _ in range(8)]
+        self.pecas_em_jogo = []
 
-    def dentro_dos_limite(self, linha, coluna):
+    def dentro_dos_limite(self, linha, coluna) -> bool:
         pass
 
-    def get_peca(self, linha, coluna):
+    def get_peca(self, linha, coluna) -> Peca:
+        pass
+
+    def get_casas_atacadas(self, linha, coluna) -> list[tuple[int,int]]:
         pass
